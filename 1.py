0c478297f0622a9931b2eb51778f81be4231da63
Dmitry <l.dimka@gmail.com>
2020-04-17 16:38:31 +0300

    new

--- 1.py
+++ 1.py
@@ -1,20 +1,17 @@
-list=[]
-list.append('bmw')
-list.append('audi')
-list.append('kia')
-list.append('hino')
-print(list)
-list.sort()
-print(list)
-print(list[1])
-list.insert(1, 'volvo')
-print(sorted(list))
-del list[0]
-print(list)
-list.sort()
-list.remove('bmw')
-print(len(list))
-list.reverse()
-print(list)
-list.insert(7, 4)
-print(list[-1])
+aliens = []
+for alien_0 in range(31):
+    new_alien = {'number': alien_0, 'color': 'green', 'points': 5+alien_0, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for all in aliens[:5]:
+    print(all)
+print(len(aliens))
+message = input("Введите значение: ")
+message = int(message)
+if message < 50:
+    print("Маловато будет")
+else:
+    print("Вот теперь норма")
+while message>=7:
+    print(f"Ждём... {message}")
+    message -= 1
